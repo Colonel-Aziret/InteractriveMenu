271471cd7b765a38709b715d1422cd001e141348
Aziret <Aziret5265@gmail.com>
2023-07-24 23:03:59 +0600

    Подключил Swagger

--- src/main/java/com/example/interactrivemenu/controller/LoginTestController.java
+++ src/main/java/com/example/interactrivemenu/controller/LoginTestController.java
@@ -1,14 +1,26 @@
 package com.example.interactrivemenu.controller;
 
+import com.example.interactrivemenu.enums.Role;
+import com.example.interactrivemenu.model.User;
+import com.example.interactrivemenu.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoginTestController
-{
+public class LoginTestController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/admin-login")
-    public String adminValidation()
-    {
+    public String adminValidation() {
         return "Admin user, Successfully logged in!";
+    }
+
+    @PostMapping("/reg")
+    public User save(@RequestBody User user) {
+        user.setRole(Role.ADMIN);
+        return userRepository.save(user);
     }
 }
